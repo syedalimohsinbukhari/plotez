41cc2be88b9780693b544c19bb60a4aef0490a7c
Syed Ali Mohsin Bukhari <ali.mohsin@ist.edu.pk>
2026-02-17 00:23:11 +0500

    [errorbar-minor-3-1]
    - corrected the `version.py` to v0.2.0

--- src/plotez/version.py
+++ src/plotez/version.py
@@ -1,6 +1,6 @@
 """Created on Feb 14 21:24:07 2026"""
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __author__ = "Syed Ali Mohsin Bukhari"
 __email__ = "ali.mohsin@ist.edu.pk"
 __license__ = "MIT"
